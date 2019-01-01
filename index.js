9974a53f567a13ba5a00874e4313fbaf59386ade
francesco <francescocasetta@gmail.com>
2019-01-01 15:30:49 +0100

    added prettier

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
 
 class Circle {
   constructor(x, y, radius) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.dx = 0;
-    this.dy = 3;
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.dx = 0
+    this.dy = 3
   }
 }
 
-Circle.prototype.update = function () {
-  this.x += this.dx;
-  this.y += this.dy;
+Circle.prototype.update = function() {
+  this.x += this.dx
+  this.y += this.dy
 
   // revert dy ?
   if (this.y + this.radius >= canvas.height || this.y - this.radius <= 0) {
     // revert
-    this.dy = -this.dy;
+    this.dy = -this.dy
   }
 
-  this.draw();
+  this.draw()
 }
 
-Circle.prototype.draw = function () {
-  ctx.beginPath();
+Circle.prototype.draw = function() {
+  ctx.beginPath()
   ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
   ctx.stroke()
 }
 
-resize();
+resize()
 
 // creating a circle
-const circle = new Circle(canvas.width / 2, canvas.height / 2, 30);
+const circle = new Circle(canvas.width / 2, canvas.height / 2, 30)
 
-requestAnimationFrame(update);
+requestAnimationFrame(update)
 
 // functions
 function update() {
-  requestAnimationFrame(update);
+  requestAnimationFrame(update)
 
   // clear canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
 
   // update circle
-  circle.update();
+  circle.update()
 }
 
 function resize() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
 }
