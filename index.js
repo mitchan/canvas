9cb76b39c3dedab2b326e37881a26c76a433fc9f
francesco <francescocasetta@gmail.com>
2019-01-01 15:17:49 +0100

    draw a circle

--- index.js
+++ index.js
@@ -2,19 +2,35 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 class Circle {
-  constructor(x, y) {
+  constructor(x, y, radius) {
     this.x = x;
     this.y = y;
+    this.radius = radius;
   }
 }
 
+Circle.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+  ctx.stroke()
+}
+
 resize();
+
+// creating a circle
+const circle = new Circle(canvas.width / 2, canvas.height / 2, 30);
 
 requestAnimationFrame(update);
 
 // functions
 function update() {
-  console.log("updating");
+  requestAnimationFrame(update);
+
+  // clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // draw a circle
+  circle.draw();
 }
 
 function resize() {
